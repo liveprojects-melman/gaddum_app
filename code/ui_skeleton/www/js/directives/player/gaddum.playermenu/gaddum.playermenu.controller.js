3add26d81dffc397dc02112bf51bfb18af5559ef
Dave Mee <davemee@gmail.com>
2019-07-02 13:15:14 +0100

    Rationalised layout, got directives working
    Moved directives into their own 'directives' folder (a sibling of 'modules') and did the same for 'services'.
    
    rationalised layout of the player directives.

---
+++ code/ui_skeleton/www/js/directives/player/gaddum.playermenu/gaddum.playermenu.controller.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('gaddum.playermenu')
+    .controller('gaddumPlayerMenuController', gaddumPlayerMenuController);
+
+  gaddumPlayerMenuController.$inject = [
+
+  ];
+
+  function gaddumPlayerMenuController(
+
+  ) {
+    var gpmc = angular.extend(this, {
+
+    });
+    return gpmc;
+  }
+})();
