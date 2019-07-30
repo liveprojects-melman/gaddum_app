feb1357e56929d6b93d79016900a8c4160fb8f8a
Dave Mee <davemee@gmail.com>
2019-07-30 14:12:38 +0100

    Player directive layout and transitions working now

--- code/www/js/services/streaming/gaddum.streaming.service.js
+++ code/www/js/services/streaming/gaddum.streaming.service.js
@@ -34,9 +34,12 @@
     // set up a thing to toggle interface from time to time!
     service.test = $interval(function(){
       service.state.playing = !service.state.playing;
-      service.state.ready = !service.state.ready;
       console.log("PLAYTOGGLE");
-    },5000,50, false );
+    },3000,50, true );
+    service.test2 = $interval(function(){
+      service.state.ready = !service.state.ready;
+      console.log("READYTOGGLE");
+    },5000,50, true );
 
     return service;
 
