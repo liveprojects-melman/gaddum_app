15a527bde2175864460e9398d99db8c2b3281901
Dave Mee <davemee@gmail.com>
2019-08-01 17:33:01 +0100

    Patching permissions as extra state on main

--- code/www/js/modules/main_ui/main_ui.module.js
+++ code/www/js/modules/main_ui/main_ui.module.js
@@ -12,12 +12,23 @@
         .state('gaddum', {
           url: '/',
           virtual: true,
+          resolve: {
+            templateNowIs: function( permissionsService ) {
+              if(permissionsService.hasAllRequiredPermissions()===true) {
+                return('js/modules/permissions/permissions.html');
+              } else {
+                return('js/modules/main_ui/main_ui.html');
+              }
+            }
+          },
 //          abstract: true,
           views: {
-            'gaddum': {
-              templateUrl: 'js/modules/main_ui/main_ui.html',
-              controller: 'main_uiController',
-              controllerAs: 'mlc'
+            'gaddum': function(templateNowIs){
+              return{
+                templateUrl: templateNowIs, //'js/modules/main_ui/main_ui.html',
+                controller: 'main_uiController',
+                controllerAs: 'mlc'
+              }
             },
             'profile@gaddum': {
               templateUrl: 'js/modules/profile/profile.html',
@@ -34,5 +45,5 @@
           cache:true,
           url: '/main/groups'
         });*/
-    })
+    });
 })();
