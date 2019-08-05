dbd795d77239877b36eab9e2ea46f6cb6b4cd589
Kei <keigibbings@live.co.uk>
2019-08-05 15:54:08 +0100

    added old main ui module

--- code/www/js/modules/main_ui/main_ui.module.js
+++ code/www/js/modules/main_ui/main_ui.module.js
@@ -12,23 +12,12 @@
         .state('gaddum', {
           url: '/',
           virtual: true,
-          resolve: {
-            templateNowIs: function( permissionsService ) {
-              if(permissionsService.hasAllRequiredPermissions()===true) {
-                return('js/modules/permissions/permissions.html');
-              } else {
-                return('js/modules/main_ui/main_ui.html');
-              }
-            }
-          },
 //          abstract: true,
           views: {
-            'gaddum': function(templateNowIs){
-              return{
-                templateUrl: templateNowIs, //'js/modules/main_ui/main_ui.html',
-                controller: 'main_uiController',
-                controllerAs: 'mlc'
-              }
+            'gaddum': {
+              templateUrl: 'js/modules/main_ui/main_ui.html',
+              controller: 'main_uiController',
+              controllerAs: 'mlc'
             },
             'profile@gaddum': {
               templateUrl: 'js/modules/profile/profile.html',
@@ -45,5 +34,5 @@
           cache:true,
           url: '/main/groups'
         });*/
-    });
+    })
 })();
