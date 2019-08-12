b1992c737265aeef14074948e35f40fc29faa16f
Kei <keigibbings@live.co.uk>
2019-08-12 09:12:03 +0100

    fix for mood service shutdown

--- code/www/js/services/gaddum.mood.service.master.switch.service.js
+++ code/www/js/services/gaddum.mood.service.master.switch.service.js
@@ -17,7 +17,8 @@
   ) {
     var service = {
       state: {
-        on: false
+        on: false,
+        count:0
       },
       turnOn: function turnOn() {
         console.log("on!");
@@ -26,14 +27,29 @@
       turnOff: function turnOff() {
         console.log("off!");
         $timeout(function(){
-          if (emotionReaderService.isRunning) {
-            emotionReaderService.setSleep(true);
-            service.state.on = false;
+          if (emotionReaderService) {
+            if (emotionReaderService.isRunning) {
+              console.log("offForReal");
+              emotionReaderService.setSleep(true);
+              service.state.on = false;
+            }
+            else{
+              if(emotionReaderService.isSleeping){
+                service.state.on = false;
+              }
+              else if(service.state.count < 10){
+                service.state.count = service.state.count+1;
+                turnOff();
+              }
+              else{
+                console.log("error", emotionReaderService);
+              }
+            }
           }
           else{
             turnOff();
           }
-        },500);
+        },1000);
         
       }
     };
